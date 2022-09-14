4cc4e13968d567e4f5d7172c0180d658fcb1a373
Anders Lunde <anders-lunde@hotmail.com>
2022-09-14 14:57:00 +0200

    Remove duplicate User(s) controller

---
+++ src/main/java/no/ssb/dapla/team/users/UserService.java
@@ -0,0 +1,4 @@
+package no.ssb.dapla.team.users;
+
+public class UserService {
+}
--- src/main/java/no/ssb/dapla/team/users/UsersController.java
+++
@@ -1,8 +0,0 @@
-package no.ssb.dapla.team.users;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UsersController {
-
-}
