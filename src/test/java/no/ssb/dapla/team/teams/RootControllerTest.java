d4d6a56564c7c7cbb73c2578d6aa6319f8ac72fd
Anders Lunde <anders-lunde@hotmail.com>
2022-09-14 21:36:10 +0200

    Add root controller test

---
+++ src/test/java/no/ssb/dapla/team/teams/RootControllerTest.java
@@ -0,0 +1,38 @@
+package no.ssb.dapla.team.teams;
+
+import no.ssb.dapla.team.RootController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(RootController.class)
+public class RootControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("$._links.self.href", is("http://localhost/")))
+                .andExpect(jsonPath("$._links.teams.href", is("http://localhost/teams")))
+                .andExpect(jsonPath("$._links.groups.href", is("http://localhost/groups")))
+                .andReturn();
+
+    }
+
+}
