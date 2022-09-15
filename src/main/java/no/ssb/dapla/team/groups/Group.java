242a55a1fcab9282819e5b7e8a86cf0ecc88051d
Anders Lunde <anders-lunde@hotmail.com>
2022-09-15 14:05:47 +0200

    Remove import Team

--- src/main/java/no/ssb/dapla/team/groups/Group.java
+++ src/main/java/no/ssb/dapla/team/groups/Group.java
@@ -2,7 +2,6 @@ package no.ssb.dapla.team.groups;
 
 import jakarta.persistence.*;
 import lombok.*;
-import no.ssb.dapla.team.teams.Team;
 import no.ssb.dapla.team.users.User;
 import jakarta.persistence.CascadeType;
 import org.hibernate.annotations.NotFound;
@@ -24,5 +23,5 @@ public class Group {
     @OneToMany(cascade = CascadeType.MERGE)
     @NotFound(action = NotFoundAction.IGNORE)
     private Set<User> users;
-
+    
 }
