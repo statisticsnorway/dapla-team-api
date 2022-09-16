9f3557fe221dbfbab68effdc0de4c77b12e4cd0a
Anders Lunde <anders-lunde@hotmail.com>
2022-09-16 12:20:43 +0200

    Use FilebasedUserService to load users

--- src/main/java/no/ssb/dapla/team/LoadDatabase.java
+++ src/main/java/no/ssb/dapla/team/LoadDatabase.java
@@ -1,11 +1,9 @@
 package no.ssb.dapla.team;
 
 import lombok.extern.slf4j.Slf4j;
-import no.ssb.dapla.team.groups.Group;
-import no.ssb.dapla.team.groups.GroupRepository;
-import no.ssb.dapla.team.teams.Team;
-import no.ssb.dapla.team.teams.TeamRepository;
+import no.ssb.dapla.team.users.FilebasedUserService;
 import no.ssb.dapla.team.users.User;
+import no.ssb.dapla.team.users.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,36 +13,12 @@ import java.util.List;
 @Configuration
 @Slf4j
 public class LoadDatabase {
-
     @Bean
-    CommandLineRunner initDatabase(TeamRepository teamRepository, GroupRepository groupRepository) {
+    CommandLineRunner initDatabase(UserRepository userRepository, FilebasedUserService filebasedUserService) {
 
         return args -> {
-
-            String[] groupsPostfixList = {"-support", "-data-admins", "-managers", "-developers"};
-            String[] teamNameList = {"demo-enhjoern-a", "demo-enhjoern-b", "demo-enhjoern-c", "demo-enhjoern-d"};
-            String[] teamDisplayNameList = {"Demo Enhjoern a", "Demo Enhjoern B", "Demo Enhjoern C", "Demo Enhjoern D"};
-            String[] names = {"Mikke", "Dolly", "Skrue", "Jokke", "Mons"};
-            String[] shortEmail = {"Mi@ssb.no", "Do@ssb.no", "Sk@ssb.no", "Jo@sbb.no", "Mons@ssb.no"};
-            String[] longEmail = {"Mikke@ssb.no", "Dolly@sbb.no", "Skrue.no", "Jokke@ssb.no", "Mons@ssb.no"};
-
-            for (int i = 0; i < teamNameList.length; i++) {
-                Team t = Team.builder()
-                        .uniformTeamName(teamNameList[i])
-                        .displayTeamName(teamDisplayNameList[i])
-                        .groups(List.of(Group.builder()
-                                .id(teamNameList[i] + groupsPostfixList[i])
-                                .users(List.of(User.builder()
-                                        .name(names[i])
-                                        .emailShort(shortEmail[i])
-                                        .email(longEmail[i]).build(), User.builder()
-                                        .name("jens")
-                                        .emailShort("mann@ssb.no")
-                                        .email("ads@ssb.no").build()))
-                                .build()))
-                        .build();
-                log.info("Preloading" + teamRepository.save(t));
-            }
+            List<User> users = filebasedUserService.fetchAllUsers();
+            for (User user : users) userRepository.save(user);
         };
     }
 }
