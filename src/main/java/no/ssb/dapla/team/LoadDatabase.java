9da29141d05bbce7a8e4f2851278132f53edbee3
Anders Lunde <Anders-lunde@hotmail.com>
2022-09-21 08:43:43 +0200

    Collect dapla teams from github api with hub4j/github-api (#11)

--- src/main/java/no/ssb/dapla/team/LoadDatabase.java
+++ src/main/java/no/ssb/dapla/team/LoadDatabase.java
@@ -1,6 +1,7 @@
 package no.ssb.dapla.team;
 
 import lombok.extern.slf4j.Slf4j;
+import no.ssb.dapla.team.github.GitHubService;
 import no.ssb.dapla.team.groups.Group;
 import no.ssb.dapla.team.teams.Team;
 import no.ssb.dapla.team.teams.TeamRepository;
@@ -11,13 +12,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
 @Slf4j
 public class LoadDatabase {
     @Bean
-    CommandLineRunner initDatabase(UserRepository userRepository, TeamRepository teamRepository, FilebasedUserService filebasedUserService) {
+    CommandLineRunner initDatabase(UserRepository userRepository, TeamRepository teamRepository, FilebasedUserService filebasedUserService, GitHubService gitHubService) {
 
         return args -> {
             // Users
@@ -25,8 +27,13 @@ public class LoadDatabase {
             userRepository.saveAll(users);
 
             // Teams
-            teamRepository.save(teamWithGroupsAndMembers("demo-enhjoern-a", "Demo Enhjørning A", users.subList(0, 5)));
-            teamRepository.save(teamWithGroupsAndMembers("demo-enhjoern-b", "Demo Enhjørning B", users.subList(5, 10)));
+            //teamRepository.save(teamWithGroupsAndMembers("demo-enhjoern-a", "Demo Enhjørning A", users.subList(0, 5)));
+            //teamRepository.save(teamWithGroupsAndMembers("demo-enhjoern-b", "Demo Enhjørning B", users.subList(5, 10)));
+            try {
+                teamRepository.saveAll(gitHubService.getRepositoryInOrganizationWithTopic("dapla-team"));
+            }catch (Exception e){
+                new RuntimeException("Could not load teams with github service");
+            }
         };
     }
 
---
+++ src/main/java/no/ssb/dapla/team/github/GitHubService.java
@@ -0,0 +1,144 @@
+package no.ssb.dapla.team.github;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.NonNull;
+import no.ssb.dapla.team.teams.Team;
+import org.kohsuke.github.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class GitHubService {
+    private final long jwtExpirationTimeInMS = 1000;
+
+    private final String appId;
+
+    private final String privateKeyPath;
+
+    private final String organizationName;
+
+    private GHOrganization ghOrganization;
+    private GHAppInstallationToken ghAppInstallationToken;
+
+
+    public GitHubService(@NonNull @Value("${github.id}") String appId,
+                         @NonNull @Value("${github.privatekey}") String privateKeyPath,
+                         @NonNull @Value("${github.organization}") String organizationName) {
+
+        this.organizationName = organizationName;
+        this.appId = appId;
+        this.privateKeyPath = privateKeyPath;
+
+        try {
+            String jwtToken = this.createJWT(appId, privateKeyPath);
+            GitHub gitHubApp = new GitHubBuilder().withJwtToken(jwtToken).build();
+
+            GHAppInstallation appInstallation = gitHubApp.getApp().getInstallationByOrganization(organizationName);
+            ghAppInstallationToken = appInstallation.createToken().create();
+            GitHub githubAuthAsInst = new GitHubBuilder().withAppInstallationToken(ghAppInstallationToken.getToken()).build();
+            ghOrganization = githubAuthAsInst.getOrganization(organizationName);
+
+        } catch (Exception e) {
+            new RuntimeException("Could not setup GitHubService");
+        }
+
+    }
+
+    private void updateTokenIfExpired() {
+        try {
+            if (ghAppInstallationToken.getExpiresAt().before(new Date())) {
+
+                String jwtToken = this.createJWT(appId, privateKeyPath);
+                GitHub gitHubApp = new GitHubBuilder().withJwtToken(jwtToken).build();
+
+
+                GHAppInstallation appInstallation = gitHubApp.getApp().getInstallationByOrganization(organizationName);
+                ghAppInstallationToken = appInstallation.createToken().create();
+                GitHub githubAuthAsInst = new GitHubBuilder().withAppInstallationToken(ghAppInstallationToken.getToken()).build();
+                ghOrganization = githubAuthAsInst.getOrganization(organizationName);
+            }
+
+        } catch (Exception e) {
+            new RuntimeException("Could not refresh token");
+        }
+    }
+
+    public List<Team> getRepositoryInOrganizationWithTopic(String topic) throws Exception {
+        updateTokenIfExpired();
+        LinkedList<Team> repositoryList = new LinkedList<>();
+            for (GHRepository ghRepository : ghOrganization.getRepositories().values()) {
+                for (String tempTopic : ghRepository.listTopics()) {
+                    if (tempTopic.equals(topic)) {
+                        repositoryList.add(Team.builder().
+                                uniformTeamName(ghRepository.getName())
+                                .displayTeamName(ghRepository.getName().replace("-", " "))
+                                .build());
+                        break;
+                    }
+
+                }
+            }
+
+        return repositoryList;
+    }
+
+    public List<GHRepository> getRepositoryInOrganizationWithNameContaining(String containing) {
+        updateTokenIfExpired();
+        List<GHRepository> repositoryList;
+        try {
+
+            repositoryList = ghOrganization.getRepositories()
+                    .values()
+                    .stream()
+                    .filter(ghRepository -> ghRepository.getName().contains(containing))
+                    .toList();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to get organizations repositories containing: " + containing);
+        }
+        return repositoryList;
+    }
+
+    private PrivateKey getPrivateKey(String filename) throws Exception {
+        byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePrivate(spec);
+    }
+
+    private String createJWT(String githubAppId, String path) throws Exception {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.RS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //Sign JWT with private key
+        Key signingKey = getPrivateKey(path);
+
+        //Set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setIssuedAt(now).setIssuer(githubAppId).signWith(signingKey, signatureAlgorithm);
+
+        //Add the expiration
+        long expMillis = nowMillis + jwtExpirationTimeInMS;
+        Date exp = new Date(expMillis);
+        builder.setExpiration(exp);
+
+        return builder.compact();
+    }
+
+}
