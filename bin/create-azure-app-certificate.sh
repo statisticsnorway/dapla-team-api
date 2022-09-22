9fe0dea6dd472884aa5b1f794d97250c3f30f850
Kenneth Leine Schulstad <ks@udp.no>
2022-09-22 10:52:33 +0200

    Update create-azure-app-certificate script

--- bin/create-azure-app-certificate.sh
+++ bin/create-azure-app-certificate.sh
@@ -3,7 +3,19 @@
 # Generate Certificate that can be registered for the AD Application
 #
 # Ref:
+# * https://github.com/Azure-Samples/ms-identity-java-daemon/tree/master/msal-client-credential-certificate
 # * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-node/docs/certificate-credentials.md
 # * https://learn.microsoft.com/en-us/azure/active-directory/develop/active-directory-certificate-credentials#register-your-certificate-with-microsoft-identity-platform
 
-openssl req -x509 -newkey rsa:2048 -keyout azure-app-private-key.key -out azure-app-certificate.crt -subj "/CN=ssb.no"
+private_path="private/azure"
+
+private_key_pem="${private_path}/azure-app-private-key.pem"
+private_key_pkcs8="${private_path}/azure-app-private-key.pkcs8"
+certificate_signing_request="${private_path}/azure-app-certificate.csr"
+certificate="${private_path}/azure-app-certificate.crt"
+
+mkdir -p ${private_path}
+openssl genrsa -out ${private_key_pem} 2048
+openssl pkcs8 -topk8 -inform PEM -outform DER -in ${private_key_pem} -nocrypt > ${private_key_pkcs8}
+openssl req -new -key ${private_key_pem} -out ${certificate_signing_request} -subj "/C=NO/CN=ssb.no/O=statisticsnorway"
+openssl x509 -req -days 365 -in ${certificate_signing_request} -signkey ${private_key_pem} -out ${certificate}
