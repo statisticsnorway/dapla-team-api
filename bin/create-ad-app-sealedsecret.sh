fb05211fd4d7bb4ab427d6c69f51b0e828537c9a
Kenneth Leine Schulstad <ks@udp.no>
2022-09-23 10:26:11 +0200

    Update scripts for generating certificates and sealed secrets

--- bin/create-ad-app-sealedsecret.sh
+++ bin/create-ad-app-sealedsecret.sh
@@ -2,16 +2,16 @@
 
 env="staging"
 platform_dev_path="$HOME/dev/code/ssb/platform-dev"
-secret_name="dapla-team-api-azure-app"
-azure_app_private_key_path="private/azure/azure-app-private-key.key"
-azure_app_certificate_path="private/azure/azure-app-certificate.crt"
+secret_name="dapla-team-api-ad-app"
+ad_app_certificate_path="private/ad/ad-app-certificate.pfx"
+ad_app_certificate_password="replaceme"
 target_file="${platform_dev_path}/flux/${env}-bip-app/dapla/dapla-team-api/${secret_name}-sealedsecret.yaml"
 
 kubectl create secret generic ${secret_name} \
 -n dapla \
 --dry-run=client \
---from-file=${azure_app_private_key_path} \
---from-file=${azure_app_certificate_path} \
+--from-file=${ad_app_certificate_path} \
+--from-literal=ad-app-certificate-password="${ad_app_certificate_password}" \
 -o yaml | kubeseal --format=yaml --cert ${platform_dev_path}/certs/${env}-bip-app.crt > ${target_file}
 
 echo "Sealed secret file: ${target_file}"
