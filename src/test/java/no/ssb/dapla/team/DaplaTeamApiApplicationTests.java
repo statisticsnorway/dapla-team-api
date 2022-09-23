92fe409b29ad8086bcf0c2125009b47ee7f41520
Anders Lunde <anders-lunde@hotmail.com>
2022-09-23 10:29:49 +0200

    Disable tests

--- src/test/java/no/ssb/dapla/team/DaplaTeamApiApplicationTests.java
+++ src/test/java/no/ssb/dapla/team/DaplaTeamApiApplicationTests.java
@@ -1,11 +1,12 @@
 package no.ssb.dapla.team;
 
 import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Ignore
+@Disabled
 class DaplaTeamApiApplicationTests {
 
     @Test
