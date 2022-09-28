9b3cec932081b2d98b1b05f0958af8a1b680dc58
Kenneth Leine Schulstad <ks@udp.no>
2022-09-28 16:25:07 +0200

    Permit all access to /api-docs

--- src/main/java/no/ssb/dapla/team/security/SecurityConfig.java
+++ src/main/java/no/ssb/dapla/team/security/SecurityConfig.java
@@ -1,9 +1,21 @@
 package no.ssb.dapla.team.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api-docs/**")
+                .permitAll();
+        return http.build();
+    }
+
 }
