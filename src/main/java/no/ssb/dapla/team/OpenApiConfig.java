a28554d1f79cf940bf56a850f228f37dddddafa8
Kenneth Leine Schulstad <ks@udp.no>
2022-09-29 23:12:18 +0200

    Add OpenAPI config

---
+++ src/main/java/no/ssb/dapla/team/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package no.ssb.dapla.team;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Dapla Team API",
+                version = "0.0.1"),
+        security = {
+                @SecurityRequirement(
+                        name = "Keycloak token")
+        })
+@SecurityScheme(
+        name = "Keycloak token",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
